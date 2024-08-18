5daea9dbcb243a19bd6041999184aa18b529af13
Rahul Meher <mikepmurphy100@gmail.com>
2024-08-19 00:55:02 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# open-fashion-backend
+ 
---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const admin = require("firebase-admin");
+const multer = require("multer");
+const bodyParser = require("body-parser");
+const path = require("path");
+
+// Start server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
